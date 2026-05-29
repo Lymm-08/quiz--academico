c3fa3f4dce8d03c8d5baa3e88b50bd4c4d49ede8
Aluno <e023aluno@outlook.com>
2026-05-28 21:45:37 -0300

    login funcionando

--- database.sql
+++ database.sql
@@ -1718,12 +1718,8 @@ BEGIN
         SELECT 'Usuário cadastrado com sucesso!' AS mensagem;
     END IF;
 
-<<<<<<< HEAD
 END;
 CREATE PROCEDURE AtualizarQuiz(IN p_id INT, IN p_pontuacao INT) BEGIN
-=======
-END // CREATE PROCEDURE AtualizarQuiz(IN p_id INT, IN p_pontuacao INT) BEGIN
->>>>>>> c0607db55e7dbd834adc1598f39bd9bf73d46c7b
 UPDATE
     quiz
 SET
@@ -1855,4 +1851,3 @@ FROM
 ORDER BY
     pontuacao DESC;
 END // DELIMITER //
-
